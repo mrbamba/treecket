c2640934f6eb9a420b39c18c90ffd638840c64f8
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-07-16 18:17:49 +0300

    built board.service.js

---
+++ src/services/board.service.js
@@ -0,0 +1,162 @@
+import httpService from './http.service'
+
+export const boardService = {
+  query,
+  getById,
+  update,
+  getEmptyBoard,
+  addBoard,
+};
+
+function query(filterBy) {
+    return httpService.get('board',filterBy)
+}
+
+function getById(boardId){
+    return httpService.get(`board/${boardId}`)
+}
+
+function update(board){
+    return httpService.put(`board/${board._id}`,board)
+}
+
+async function addBoard(board){
+    return await httpService.post(`board`,board)
+}
+
+function getEmptyBoard(){
+    return {
+        "_id": "qeovinerv",
+        "name": "FrontEnd",
+        "background": "#fff",
+        "nextId":{
+            "activities":"a101",
+            "labels":"l101",
+            "groups":"g101",
+            "tickets":"t101"
+        },
+        "members": [
+            {
+                "_id": "dfverwli3g43",
+                "fullName": "user2",
+                "imgSrc": "img//user/img2"
+            }
+        ],
+        "labels": [
+            {
+                "id": 100,
+                "color": "4a4",
+                "name": "UX"
+            }
+        ],
+        "activities": [
+            {
+                "id": 100,
+                "txt": "Tal edited ticket 234876 title",
+                "createdAt": 1234567890,
+                "ticketId": "4tbg2354f",
+                "by": {
+                    "_id": "u101",
+                    "fullName": "user1",
+                    "imgSrc": "img//user/img1"
+                }
+            }
+        ],
+        "groups": [
+            {
+                "id": 100,
+                "name": "QA",
+                "color": "#fff",
+                "tickets": [
+                    {
+                        "id": 100,
+                        "title": "",
+                        "description": "",
+                        "color": "#fff",
+                        "members": [
+                            {
+                                "_id": 100,
+                                "fullName": "user1",
+                                "imgSrc": "img//user/img1"
+                            },
+                            {
+                                "_id": 101,
+                                "fullName": "user2",
+                                "imgSrc": "img//user/img2"
+                            }
+                        ],
+                        "labels": [
+                            101,
+                            102
+                        ],
+                        "createdAt": 1234567890,
+                        "by": {
+                            "_id": 100,
+                            "fullName": "user1",
+                            "imgSrc": "img//user/img1"
+                        },
+                        "comments": [
+                            {
+                                "id":100,
+                                "txt": "Need to push this to next sprint",
+                                "createdAt": 1234567890,
+                                "by": {
+                                    "_id": "u101",
+                                    "fullName": "user1",
+                                    "imgSrc": "img//user/img1"
+                                }
+                            }
+                        ],
+                        "checklists": [
+                            {
+                                "id":100,
+                                "title": "UX",
+                                "items": [
+                                    {
+                                        "txt": "some to do",
+                                        "isDone": true
+                                    },
+                                    {
+                                        "txt": "some to do",
+                                        "isDone": false
+                                    }
+                                ]
+                            },
+                            {
+                                "id":101,
+                                "title": "Backend",
+                                "items": [
+                                    {
+                                        "txt": "some to do",
+                                        "isDone": true
+                                    },
+                                    {
+                                        "txt": "some to do",
+                                        "isDone": false
+                                    }
+                                ]
+                            }
+                        ],
+                        "attachments": [
+                            {
+                                "id":100,
+                                "src": "https://bla.blabla.com/shmulik.jpg",
+                                "type": "img"
+                            },
+                            {
+                                "id":101,
+                                "src": "https://bla.blabla.com/",
+                                "type": "link"
+                            },
+                            {
+                                "id":102,
+                                "src": "https://bla.blabla.com/",
+                                "type": "video"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
