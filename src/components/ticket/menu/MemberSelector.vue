56d4e2be5c3d330d7633c339c547777a25dd4af6
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-07-17 14:37:58 +0300

    member selection work

---
+++ src/components/ticket/menu/MemberSelector.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="member-selector">
+        <h2>Members</h2>
+        <input type="search" />
+        <h4>BOARD MEMBERS</h4>
+        <ul>
+            <li v-for="user in users" :key="user._id">
+                <pre>{{user}}</pre>
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MemberSelector',
+    props: {
+        users: {
+            type: Array,
+            require: true
+        }
+    }
+
+}
+</script>
+
+<style>
+</style>
