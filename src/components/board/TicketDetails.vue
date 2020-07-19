aa3063f291b9d805e5ea226d224e53ab0c9d02da
yonik94 <yehonatan121233@gmail.com>
2020-07-19 22:39:23 +0300

    TicketCheclists componenet starter

--- src/components/board/TicketDetails.vue
+++ src/components/board/TicketDetails.vue
@@ -2,24 +2,29 @@
     <div class="ticket-details">
         <button class="close-btn" @click="closeTicketDetails">X</button>
         <div>
-        <textarea @input="expandTextareaEl()" ref="title" v-model="ticket.title" @blur="saveTicket" maxlength="140" />
-        <section>
-            <h3>Description:</h3>
-            <textarea v-model="ticket.description" @blur="saveTicket" placeholder="Enter your ticket description"
-            cols="30" rows="10"/>
-            <section v-for="attachment in ticket.attacments" :key="attachment.id">
-                {{ attachment }}
+            <textarea
+                @input="expandTextareaEl()"
+                ref="title"
+                v-model="ticket.title"
+                @blur="saveTicket"
+                maxlength="140"
+            />
+            <section>
+                <h3>Description:</h3>
+                <textarea
+                    v-model="ticket.description"
+                    @blur="saveTicket"
+                    placeholder="Enter your ticket description"
+                    cols="30"
+                    rows="10"
+                />
+                <section
+                    v-for="attachment in ticket.attacments"
+                    :key="attachment.id"
+                >{{ attachment }}
+                </section>
+                <ticket-checklists :ticket="ticket" />
             </section>
-            <section v-for="checklist in ticket.checklists" :key="checklist.id">
-                Checklist id: {{ checklist.id }}
-                <ul>
-                    <li v-for="(item, idx) in checklist.items" :key="idx">
-                        <input type="checkbox" v-model="item.isDone" />
-                        <h4 :class="{done: item.isDone}">{{ item.txt }}</h4>
-                    </li>
-                </ul>
-            </section>
-        </section>
         </div>
         <ticket-menu @deleteTicket="deleteTicket" :ticket="ticket" />
     </div>
@@ -27,28 +32,30 @@
 
 <script>
 import TicketMenu from "@/components/ticket/TicketMenu.vue";
+import TicketChecklists from "@/components/ticket/TicketChecklists.vue";
 export default {
     props: ['ticket', 'groupId'],
     // data(){
-        // return {
-        //     description: this.ticket.description
-        // }
+    // return {
+    //     description: this.ticket.description
+    // }
     // },
     components: {
-        TicketMenu
+        TicketMenu,
+        TicketChecklists
     },
     methods: {
-        saveTicket(){
+        saveTicket() {
             this.$emit('saveTicket', this.ticket)
         },
-        closeTicketDetails(){
+        closeTicketDetails() {
             this.$emit('closeTicketDetails')
         },
-        deleteTicket(ticketId){
+        deleteTicket(ticketId) {
             console.log('TicketDetails params:', ticketId, this.groupId)
             this.$emit('deleteTicket', { ticketId, groupId: this.groupId })
         },
-        expandTextareaEl(){
+        expandTextareaEl() {
             const el = this.$refs.title;
             el.style.height = ""; el.style.height = el.scrollHeight + "px"
         }
@@ -57,7 +64,7 @@ export default {
 </script>
 
 <style>
-    .done{
-        text-decoration: line-through;
-    }
+.done {
+    text-decoration: line-through;
+}
 </style>
