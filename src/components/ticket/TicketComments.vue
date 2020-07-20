116269a5e249dc6134ee1bea08245c3334a59821
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-07-20 17:50:19 +0300

    initial ticketComment setup

---
+++ src/components/ticket/TicketComments.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="ticket-footer">
+        <div>
+            <font-awesome-icon fas icon="history" />
+            <h3>Show:</h3>
+        </div>
+        <button>Comments</button>
+        <button>History</button>
+    </div>
+</template>
+
+<script>
+  import Avatar from 'vue-avatar-component'
+
+export default {
+    name:'TicketComments',
+    data(){
+        return{
+            showing:'Comments'
+        }
+    }
+
+};
+</script>
+
+<style>
+</style>
