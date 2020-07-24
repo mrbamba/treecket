3c00eadc29f148031e3e27b0c0dfa451862b5c4a
talshachar <taltalsh97@gmail.com>
2020-07-25 00:14:31 +0300

    dashboard starter

--- src/components/board/Dashboard.vue
+++ src/components/board/Dashboard.vue
@@ -1,43 +1,23 @@
 <template>
     <section class="dashboard">
-        <line-chart :styles="myStyles" />
-        <button @click="increase()">Increase height</button>
+        <group-map :board="board" />
     </section>
 </template>
 
 <script>
-import VueCharts from 'vue-chartjs'
-import { Bar, Line } from 'vue-chartjs'
+import GroupMap from '@/components/board/charts/GroupMap.vue';
+
 export default {
     name: "Dashboard",
     props: {
         board: {
             type: Object,
             require: true
-        }
-    },
-    data() {
-        return {
-            height: 300
-        }
-    },
-    created() {
-        console.log(this.board);
-    },
-    methods: {
-        increase() {
-            this.height += 10
         },
     },
-    computed: {
-        myStyles() {
-            return {
-                height: `${this.height}px`,
-                position: 'relative'
-            }
-        }
+    components: {
+        GroupMap,
     }
-
 }
 </script>
 
---
+++ src/components/board/charts/GroupMap.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="dashboard">
+        <bar :chartdata="chartdata" :options="options" />
+        <!-- <button @click="increase()">Increase height</button> -->
+    </section>
+</template>
+
+<script>
+import VueCharts from 'vue-chartjs'
+import { Bar } from 'vue-chartjs'
+
+export default {
+    name: "Dashboard",
+    extends: Bar,
+    data() {
+        return {
+            chartdata: {
+                labels: ['TO DO', 'IN PROGRESS', 'DONE'],
+                datasets: [
+                    {
+                        label: 'Data One',
+                        backgroundColor: '#f87979',
+                        data: [18, 7, 43]
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }
+    },
+    // props: {
+    // board: {
+    //     type: Object,
+    //     require: true
+    // },
+    // chartdata: {
+    //     type: Object,
+    //     default: null
+    // },
+    // options: {
+    //     type: Object,
+    //     default: null
+    // }
+    // },
+    mounted() {
+        this.renderChart(this.chartdata, this.options)
+    },
+    components: {
+        Bar
+    }
+}
+</script>
