dc10472a7475148c1ecd6cc38d0d31ef5a78337d
Dan Haski <6283385+mrbamba@users.noreply.github.com>
2020-07-25 17:43:43 +0300

    more graphs

---
+++ src/components/board/charts/TicketsPerMemberChart.vue
@@ -0,0 +1,31 @@
+<script>
+import { Pie } from 'vue-chartjs'
+export default {
+    name:'TicketsPerMemberChart',
+    extends: Pie,
+    props: {
+        label: {
+            type: String,
+        },
+        chartData: {
+            type: Object,
+        },
+        options: {
+            type: Object,
+        }
+    },
+    mounted() {
+        const members = this.chartData.fullName
+        const ticketCount = this.chartData.ticketCount
+
+        this.renderChart({
+            labels: members,
+            datasets: [{
+                label: this.label,
+                data: ticketCount
+            }]
+        }, this.options)
+    }
+}
+
+</script>
