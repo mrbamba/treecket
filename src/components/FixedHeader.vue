6cb16c1264a07ec87f6c25015a65a29fe6870342
talshachar <taltalsh97@gmail.com>
2020-07-26 16:10:40 +0300

    home starter

---
+++ src/components/FixedHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="fixed-header">
+            <div>
+                <div class="logo">
+                    <img src="@/assets/logo.png" />
+                    <h1>Treecket</h1>
+                </div>
+            </div>
+            <div class="user-options">
+                <router-link to="/login" class="cancel-button">Login</router-link>
+                <router-link to="/signup" class="add-button">Signup</router-link>
+            </div>
+        </header>
+</template>
+
+<script>
+export default {
+    name: "BoardPreview",
+    props: {
+        board: {
+            type: Object,
+            require: true
+        }
+    },
+    // computed: {
+    //     background() {
+    //         // return this.board.background + ((this.board.background.includes('url')) ? 'center / cover' : '');
+    //     }
+    // }
+}
+</script>
