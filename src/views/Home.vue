de4abe8b7844e292f22ebf4da2de1af5543d7815
talshachar <taltalsh97@gmail.com>
2020-07-26 16:39:53 +0300

    home page for demo and fixed header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,19 +3,19 @@
         <fixed-header />
         <section class="jumbo-white">
             <div>
-                <h2>The best tool for agile software development</h2>
+                <h2>Probably the best tool for agile software development</h2>
                 <router-link to="/board" class="save-button">Try it free</router-link>
             </div>
             <img src="@/assets/icons/scrum-board.png" alt />
         </section>
-        
+
         <section class="jumbo-gray">
             <h3>Items are represented visually on a kanban board, allowing team members to see the state of every piece of work at any time.</h3>
         </section>
 
         <section class="jumbo-white">
             <img src="@/assets/icons/note-list-mobile.png" alt />
-            <h2>Real-time communication of capacity and full transparency of work.</h2>
+            <h2>Real-time communication and full transparency.</h2>
         </section>
     </div>
 </template>
