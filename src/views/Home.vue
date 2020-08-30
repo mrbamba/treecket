a630f4075b7ac8fe221dd2dea30171e615fbe0b6
talshachar <taltalsh97@gmail.com>
2020-08-30 18:43:49 +0300

    implementing inline helpers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,39 +5,45 @@
             <section class="trial-heading">
                 <div>
                     <h2>
-                        <span>Everything Starts With</span> 
+                        <span>Everything Starts With</span>
                         <span>A Great Kanban</span>
                     </h2>
                     <router-link to="/board" class="trial-btn-main">Try it free</router-link>
                 </div>
             </section>
 
-            <section class="jumbo-white">
-                <div class="img-hero-container">
-                    <img src="@/assets/images/note-list-mobile.svg" alt />
-                </div>
-                <div>
-                    <h2>Real-time communication and full transparency</h2>
-                    <h3>Items are represented visually on a kanban board, allowing team members to see the state of every piece of work at any time.</h3>
-                </div>
+            <section class="jumbo">
+                <!-- <div class="container"> -->
+                    <div class="img-hero-container">
+                        <img src="@/assets/images/note-list-mobile.svg" alt />
+                    </div>
+                    <div>
+                        <h2>Real-Time Communication, Full Transparency</h2>
+                        <h3>Items are represented visually on a kanban board, allowing team members to see the state of every piece of work at any time.</h3>
+                    </div>
+                <!-- </div> -->
             </section>
-            
-            <!-- <section class="jumbo-gray">
-                <div>
-                    <h3>Items are represented visually on a kanban board, allowing team members to see the state of every piece of work at any time.</h3>
-                </div>
-                <div class="img-hero-container" style="display: block">
-                    <img src="@/assets/images/scrum-board.svg" alt />
-                </div>
-            </section> -->
 
-            <section class="jumbo-white">
-                <div>
-                    <h2>Probably the best tool for agile software development</h2>
-                </div>
-                <div class="img-hero-container">
-                    <img src="@/assets/images/dashboard.svg" alt />
-                </div>
+            <section class="jumbo">
+                <!-- <div class="container"> -->
+                    <div>
+                        <h2>No Brainer Agile Development Mangement Tool</h2>
+                    </div>
+                    <div class="img-hero-container">
+                        <img src="@/assets/images/prototyping-process.svg" alt />
+                    </div>
+                <!-- </div> -->
+            </section>
+
+            <section class="jumbo">
+                <!-- <div class="container"> -->
+                    <div class="img-hero-container">
+                        <img src="@/assets/images/dashboard.svg" alt />
+                    </div>
+                    <div>
+                        <h2>No Brainer Agile Development Mangement Tool</h2>
+                    </div>
+                <!-- </div> -->
             </section>
         </main>
     </div>
